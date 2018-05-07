6d3c3b6afea1c39343b1fbe8c469eb69f36867ec
mnamias <38704586+mnamias@users.noreply.github.com>
2018-05-07 12:15:22 -0300

    Add files via upload
    7/5/2018
    CRCs figure: set default filter value o zero (none)
    Fixed uncommented outputs to console

--- harmonization_toolbox.m
+++ harmonization_toolbox.m
@@ -31,7 +31,7 @@
 %   license.txt.
 %
 % Overview: This is a GUI for the paper "A novel
-% approach for quantitative harmonization_menu in PET" by M. Namías et. al., PMB, May 2018
+% approach for quantitative harmonization_menu in PET" M Namías et al 2018 Phys. Med. Biol. 63 095019
 %
 % Inputs and ranges of inputs: the only input is a set of DICOM files of a
 % cylindrical phantom.
@@ -300,11 +300,7 @@ global flag_NPS
 options.mode = 'manual';
 [Wavg, Savg, rbin, npsr, npsr_norm, avg_std, mean_bkg, psf_noise, xaxis, yaxis, zaxis] = NPScalc( image , dicom_headers , options);
 
-        % plot the result
-        axes(handles.axes16);
-        psf_mip = mipss(psf_noise);
-        imshow(-psf_mip,[-max(psf_mip(:)) 0])
-        
+            
         axes(handles.axes13);
         subplot(1,3,1);
         plot(xaxis,Wavg(:,ceil(end/2),ceil(end/2)),'k.-');
@@ -343,7 +339,7 @@ function about_Callback(hObject, eventdata, handles)
 
 disclaimer = 'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.';
 
-h = msgbox( {'PET Harmonization Toolbox v1.0 (April 23, 2018).' ' ' 'Copyright 2018 Mauro Namías (mnamias@gmail.com)' ' ' 'This toolbox is to accompany the method published in PMB 2018 "A novel approach for quantitative harmonization in PET" by M. Namías et. al.' ' ' disclaimer} ); 
+h = msgbox( {'PET Harmonization Toolbox v1.0 (April 23, 2018).' ' ' 'Copyright 2018 Mauro Namías (mnamias@gmail.com)' ' ' 'This toolbox is to accompany the method published in PMB 2018 "A novel approach for quantitative harmonization in PET" M Namías et al 2018 Phys. Med. Biol. 63 095019.' ' ' disclaimer} ); 
  
 
 % --------------------------------------------------------------------
